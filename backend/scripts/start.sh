4fdd9f958e47c87fdeda3dbde88c8a0942f2c78c
park young jae <jolie0215@gmail.com>
2023-03-05 10:18:54 +0900

    supervisor 도입

--- backend/scripts/start.sh
+++ backend/scripts/start.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 
+
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput --verbosity 0
 
+service supervisor start # 이걸 해줘야 supervisor가 정상적으로 동작한다.
+
+#supervisorctl reread
+#supervisorctl update
+
+#supervisorctl start gunicorn
+#supervisorctl start daphne
+
 #daphne -b 0.0.0.0 -p 8001 config.asgi:application -v2 &
 #python manage.py runserver 0.0.0.0:8000 #-v2
 
-gunicorn -c config/gunicorn.conf.py config.wsgi:application &&
-daphne -b 0.0.0.0 -p 8001 config.asgi:application
+#gunicorn -c config/gunicorn.conf.py config.wsgi:application &&
+#daphne -b 0.0.0.0 -p 8001 config.asgi:application
