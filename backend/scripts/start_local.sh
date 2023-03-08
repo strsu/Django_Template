5db6219cec1ccd894a305f24dd844a638dd23b5a
park young jae <jolie0215@gmail.com>
2023-03-08 23:57:14 +0900

    Feat - local 개발 환경 구축

---
+++ backend/scripts/start_local.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+
+python manage_local.py makemigrations
+python manage_local.py migrate
+python manage_local.py collectstatic --noinput --verbosity 0
+
+nohup service supervisor start & # 이걸 해줘야 supervisor가 정상적으로 동작한다.
+python manage_local.py runserver 0.0.0.0:8000
