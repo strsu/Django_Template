a58561d0e9467f76b85af2fb9e18a7cdd78a4124
park young jae <jolie0215@gmail.com>
2023-03-29 00:16:45 +0900

    Feat - ssl 적용

--- backend/api/v1/chat/routing.py
+++ backend/api/v1/chat/routing.py
@@ -1,9 +1,12 @@
 from django.urls import re_path
 
-from api.v1.chat import consumer
+from api.v1.chat import consumer, androidConsumer
 
 websocket_urlpatterns = [
     re_path(
         r"^ws/chat/(?P<room_name>[^/]+)/$", consumer.ChatConsumer.as_asgi()
     ),  # as_asgi()는 장고의 as_view()와 같은 역할을 한다
+    re_path(
+        r"^ws/mchat/(?P<room_name>[^/]+)/$", androidConsumer.AndroidConsumer.as_asgi()
+    ),
 ]
