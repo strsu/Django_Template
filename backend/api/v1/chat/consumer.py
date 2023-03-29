6b0ef3e24cd598d1d77c06a16ddb9e56a583040e
park young jae <jolie0215@gmail.com>
2023-03-30 00:52:27 +0900

    Chore - 소켓 메시지 테스트

--- backend/api/v1/chat/consumer.py
+++ backend/api/v1/chat/consumer.py
@@ -1,6 +1,8 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 
+# https://blog.logrocket.com/django-channels-and-websockets/
+
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -21,10 +23,31 @@ class ChatConsumer(AsyncWebsocketConsumer):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
 
-        # Send message to room group
+        if message:
+            text = message["text"]
+            await self.send_to_other_members(text)
+
+        return
+
+        # Send message to room group - 나를 포함 모든 멤버
         await self.channel_layer.group_send(
             self.room_group_name, {"type": "chat_message", "message": message}
         )
+
+    async def send_to_other_members(self, message):
+        members = await self.get_group_members(self.group_name)
+        for member in members:
+            f = open("/opt/text.log", "a", encoding="utf-8")
+            f.write(member + "\n")
+            f.close()
+            if member != self.channel_name:
+                await self.channel_layer.send(
+                    member,
+                    {
+                        "type": "chat_message",
+                        "message": message,
+                    },
+                )
 
     # Receive message from room group
     async def chat_message(self, event):
@@ -32,3 +55,15 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         # Send message to WebSocket
         await self.send(text_data=json.dumps({"message": message}))
+
+    @staticmethod
+    async def get_group_members(group_name):
+        """
+        Get list of members from the channel layer for a specific group
+        """
+        channel_layer = ChatConsumer.channel_layer
+        group = await channel_layer.group_get(group_name)
+        if group:
+            return group.keys()
+        else:
+            return []
