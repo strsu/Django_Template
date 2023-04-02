d795d2cd50208b9e56b763be4e6d909f4f4232ac
park young jae <jolie0215@gmail.com>
2023-04-02 18:57:07 +0900

    Feat - 여러가지 기능 추가

--- backend/api/v1/chat/consumer_user.py
+++ backend/api/v1/chat/consumer_user.py
@@ -1,6 +1,11 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 from config.settings_local import logger_info
 import json
+import random
+import string
+
+from api.v1.chat.service.food_recommand import foodRecommand
+from api.v1.chat.service.user_counter import userCounter
 
 
 # https://blog.logrocket.com/django-channels-and-websockets/
@@ -9,7 +14,17 @@ import json
      -> websocket 특성이라는 것 같음
      
     그래서 코드를 반영하려면 dephan을 restart해줘야 한다.
+    
+    consumer는 사용자 마다 1개씩 부여되는 것 같다.
+     -> 접속자 n 명 = consumer n개
+    
+    channel_layer = RedisChannelLayer
 """
+
+
+def generate_random_string(length):
+    letters = string.ascii_lowercase
+    return "".join(random.choice(letters) for i in range(length))
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
@@ -18,61 +33,80 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.user_name = self.scope["url_route"]["kwargs"]["user_name"]
         self.room_group_name = "chat_%s" % self.room_name
 
-        logger_info.info(f"{self.room_name} - {self.user_name}")
+        # logger_info.info(str(self.scope["headers"]))
+
+        # 사용자 현황
+        token = generate_random_string(10)
+        self.uc = userCounter(self.room_group_name)
+        await self.uc.connect()
+
+        # 음식 추천
+        self.fr = foodRecommand()
 
         # Join room group
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
-
         await self.accept()
+        await self.user_in()
 
     async def disconnect(self, close_code):
         # Leave room group
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+        await self.user_out()
+        await self.uc.close()
 
     # Receive message from WebSocket
-    async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
-        name = text_data_json["name"]
+    async def receive(self, text_data=None):
 
-        # if message:
-        #     text = message["text"]
-        #     await self.send_to_other_members(text)
+        text_data_json = json.loads(text_data)
+        data = {
+            "type": "chat_message",
+            "data": text_data_json["data"],
+        }
 
         # Send message to room group - 나를 포함 모든 멤버
+
+        if "오늘뭐먹지" in data["data"]["message"].replace(" ", ""):
+            data["data"] = {
+                **data["data"],
+                "message": self.fr.get_random_store(),
+                "name": "음식추천해줌",
+                "token": "",
+            }
+
+        """
+            1. receive 가 호출됨
+            2. type : chat_message
+                -> chat_message 함수가 호출됨
+        """
         await self.channel_layer.group_send(
             self.room_group_name,
-            {"type": "chat_message", "message": message, "name": name},
+            data,
         )
 
-    async def send_to_other_members(self, message):
-        members = await self.get_group_members(self.group_name)
-        for member in members:
-            if member != self.channel_name:
-                await self.channel_layer.send(
-                    member,
-                    {
-                        "type": "chat_message",
-                        "message": message,
-                    },
-                )
+    async def user_in(self):
+        count = await self.uc.user_in()
+        await self.send_user_count(count)
+
+    async def user_out(self):
+        count = await self.uc.user_out()
+        await self.send_user_count(count)
+
+    async def send_user_count(self, count):
+        data = {
+            "type": "info_message",
+            "data": {"user_cnt": count},
+        }
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            data,
+        )
 
     # Receive message from room group
     async def chat_message(self, event):
-        message = event["message"]
-        name = event["name"]
-
         # Send message to WebSocket
-        await self.send(text_data=json.dumps({"message": message, "name": name}))
+        await self.send(text_data=json.dumps({"msg": event["data"]}))
 
-    @staticmethod
-    async def get_group_members(group_name):
-        """
-        Get list of members from the channel layer for a specific group
-        """
-        channel_layer = ChatConsumer.channel_layer
-        group = await channel_layer.group_get(group_name)
-        if group:
-            return group.keys()
-        else:
-            return []
+    # Receive message from room group
+    async def info_message(self, event):
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({"info": event["data"]}))
---
+++ backend/api/v1/chat/service/user_counter.py
@@ -0,0 +1,38 @@
+import aioredis
+from config.settings_local import logger_info
+
+
+class userCounter:
+    def __init__(self, cache_key):
+        self.redis = None
+        self.cache_key = cache_key
+
+    async def connect(self):
+        self.redis = await aioredis.from_url("redis://redis")
+        await self.redis.set(self.cache_key, 1)
+        # key_list = await self.redis.keys(f"{self.cache_key}*")
+
+    async def close(self):
+        self.redis.close()
+
+    async def user_in(self):
+        # Redis 캐시에서 데이터를 가져옵니다.
+        count = await self.redis.get(self.cache_key)
+        if not count:
+            await self.redis.set(self.cache_key, 1)
+            return 1
+        else:
+            await self.redis.set(self.cache_key, int(count.decode("utf-8")) + 1)
+
+        return int(count.decode("utf-8")) + 1
+
+    async def user_out(self):
+        # Redis 캐시에서 데이터를 가져옵니다.
+        count = await self.redis.get(self.cache_key)
+        if not count:
+            await self.redis.set(self.cache_key, 0)
+            return 0
+        else:
+            await self.redis.set(self.cache_key, int(count.decode("utf-8")) - 1)
+
+        return int(count.decode("utf-8")) - 1
