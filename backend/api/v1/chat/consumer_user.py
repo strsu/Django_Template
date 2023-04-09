9152753670e6678e3c647cd88912650fd583a315
park young jae <jolie0215@gmail.com>
2023-04-09 16:07:51 +0900

    Feat - 파일 전송 및 사용자 정보 감추기 기능 추가

--- backend/api/v1/chat/consumer_user.py
+++ backend/api/v1/chat/consumer_user.py
@@ -3,10 +3,12 @@ from config.settings.base import logger_info
 import json
 import random
 import string
+import copy
+from datetime import datetime
 
 from api.v1.chat.service.food_recommand import foodRecommand
 from api.v1.chat.service.user_counter import userCounter
-from api.v1.chat.service.file_saver import save_image
+from api.v1.chat.service.file_saver import save_base64, save_bytes
 
 
 # https://blog.logrocket.com/django-channels-and-websockets/
@@ -23,12 +25,18 @@ from api.v1.chat.service.file_saver import save_image
 """
 
 
+def generate_random_string(length):
+    letters = string.ascii_letters + string.digits
+    return "".join(random.choice(letters) for _ in range(length))
+
+
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_name = "mzoffice"
         self.user_name = self.scope["url_route"]["kwargs"]["user_name"]
         self.room_group_name = "chat_%s" % self.room_name
+        self.user_token = generate_random_string(10)
 
         # logger_info.info(str(self.scope["headers"]))
 
@@ -51,19 +59,45 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.uc.close()
 
     # Receive message from WebSocket
-    async def receive(self, text_data=None):
+    async def receive(self, text_data=None, bytes_data=None):
+        """
+        receive는 data를 전송한 consumer만 실행되는 것 같다.
+        이후에 아래서 정리된 data가 redis에 올라가고
+        data를 받아야 하는 그룹원들은
+        redis에 올라온 data를 가져와 type에 명시된 함수를 실행하는 것 같다.
+        """
 
-        text_data_json = json.loads(text_data)
+        if text_data is not None:
+            data = await self.text_receive(text_data)
+        else:
+            data = await self.bytes_receive(bytes_data)
+
+        if data:
+            data["data"]["token"] = self.user_token
+
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                data,
+            )
+
+    async def text_receive(self, raw_data):
+        text_data_json = json.loads(raw_data)
         data = {
             "type": "chat_message",
             "data": text_data_json["data"],
         }
 
-        if text_data_json["data"]["image"]:
-            save_image(text_data_json["data"]["image"], self.room_name)
+        if "flag" in text_data_json["data"]:
+            flag = text_data_json["data"]["flag"]
+            filename = text_data_json["data"]["file"]
+            await save_bytes(None, self.room_name, flag, filename)
 
-        # Send message to room group - 나를 포함 모든 멤버
-        if "오늘뭐먹지" in data["data"]["message"].replace(" ", ""):
+            if flag:
+                return False
+        elif "image" in text_data_json["data"]:
+            save_base64(text_data_json["data"]["image"], self.room_name)
+        elif "오늘뭐먹지" in data["data"]["message"].replace(" ", ""):
+            # Send message to room group - 나를 포함 모든 멤버
             data = {
                 "type": "food_message",
                 "data": {
@@ -74,15 +108,11 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 },
             }
 
-        """
-            1. receive 가 호출됨
-            2. type : chat_message
-                -> chat_message 함수가 호출됨
-        """
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            data,
-        )
+        return data
+
+    async def bytes_receive(self, raw_data):
+        await save_bytes(raw_data, self.room_name, flag=2)
+        return False
 
     async def user_in(self):
         count = await self.uc.user_in()
@@ -102,13 +132,47 @@ class ChatConsumer(AsyncWebsocketConsumer):
             data,
         )
 
-    # Receive message from room group
+    async def hello(self):
+        data = {
+            "type": "info_message",
+            "data": {"user_token": self.user_token},
+        }
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            data,
+        )
+
     async def chat_message(self, event):
-        # Send message to WebSocket
-        await self.send(text_data=json.dumps({"msg": event["data"]}))
+        # consumer와 연결된 사용자한테 데이터를 전송해준다.
+        # 내부적으로 token을 검사해서, flag로 변경시킨다.
+        """
+        들어온 순서대로 data가 전송된다.
+        들어온 data는 연결된 user에게 전송된 후
+        다시 redis로 들어간다.
+            -> 이건 정확한 로직을 모르겠다.
+        그래서 전송하면서 각 user의 consumer가 data를 변조하면
+        이 그후에 값을 받는 사용자들은 변조된 값을 받게 된다.
+            -> 그럼 redis에서 가져온 객체를 사용하지 않고,
+            -> deepcopy를 해서 사용하면 원본 객체는 손상시키지 않기 때문에
+            -> 숨기고 싶은 데이터를 숨길 수 있다.
+
+        또한 3번 user가 보낸 data는 3번 유저가 먼저 받는게 아니라
+        1, 2번 유저가 받고 그 이후에 3번 유저가 data를 받게 된다.
+        """
+
+        data = copy.deepcopy(event["data"])
+        if data["token"] == self.user_token:
+            data["flag"] = True
+        else:
+            data["flag"] = False
+        del data["token"]
+
+        await self.send(text_data=json.dumps({"msg": data}))
 
     async def info_message(self, event):
         await self.send(text_data=json.dumps({"info": event["data"]}))
 
     async def food_message(self, event):
+        if "token" in event["data"]:
+            del event["data"]["token"]
         await self.send(text_data=json.dumps({"food": event["data"]}))
--- backend/api/v1/chat/service/file_saver.py
+++ backend/api/v1/chat/service/file_saver.py
@@ -1,15 +1,44 @@
+import os
 import time
 import base64
 
+from config.settings.base import STATIC_ROOT
 
-def save_image(base64_string, room):
+
+def mkdir(path):
+    os.makedirs(path, exist_ok=True)
+
+
+def save_base64(base64_string, room):
+    path = os.path.join(STATIC_ROOT, f"chat/img/{room}")
+    mkdir(path)
     # 데이터 부분 분리
     # base64_string = "data:image/jpeg;base64,/9j/4AAQSkZJRg
     ext, img_data = base64_string.split(",")
     ext = ext.split("/")[1][:-7]
+    filename = f"{room}_{str(time.time())}.{ext}"
     # decode base64 string data
     decoded_data = base64.b64decode((img_data))
     # write the decoded data back to original format in  file
-    img_file = open(f"/opt/{room}_{str(time.time())}.{ext}", "wb")
+    img_file = open(os.path.join(path, filename), "wb")
     img_file.write(decoded_data)
     img_file.close()
+
+
+async def save_bytes(raw, room, flag, filename="jj"):
+    path = os.path.join(STATIC_ROOT, f"chat/img/{room}")
+    mkdir(path)
+    if flag == 2:
+        # 파일 전송 중
+        img_file = open(os.path.join(path, filename), "ab")
+        img_file.write(raw)
+        img_file.close()
+    elif flag == 1:
+        # 파일 전송 시작
+        filename = "jj"
+        img_file = open(os.path.join(path, filename), "wb")
+        img_file.close()
+    elif flag == 0:
+        # 파일 전송 종료
+        filename = f"{room}_{str(time.time())}_{filename}"
+        os.rename(os.path.join(path, "jj"), os.path.join(path, filename))
