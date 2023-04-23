0b4857970f7bbe2206594ceb77db07caec080b5e
park young jae <jolie0215@gmail.com>
2023-04-23 18:13:58 +0900

    Feat - 방, 사용자 이름 post 처리

--- backend/api/v1/chat/views.py
+++ backend/api/v1/chat/views.py
@@ -1,26 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from config.settings.base import logger_info
 import time
 
-from rest_framework.decorators import permission_classes
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import status, generics, mixins
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.authentication import BasicAuthentication
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 
-def index(request):
-    return render(request, "chat/index.html", {})
+class ChatApiView(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request):
+        return render(request, "chat/index.html", {})
 
 
-def room(request, room_name):
-    return render(request, "chat/room.html", {"room_name": room_name})
+class ChatPlayApiView(APIView):
+    permission_classes = [AllowAny]
 
+    def get(self, request):
+        return redirect(f"/api/chat/")
 
-def roomWithUser(request, room_name, user_name):
-    if room_name == "mafia":
-        return render(
-            request,
-            "chat/roomMafia.html",
-        )
-    else:
+    def post(self, request):
+        room_name = request.data.get("room_name")
+        user_name = request.data.get("user_name")
         return render(
             request,
             "chat/roomWithUser.html",
@@ -28,9 +35,8 @@ def roomWithUser(request, room_name, user_name):
         )
 
 
-def roomRandom(request):
-    return render(
-        request,
-        "chat/roomRandom.html",
-        # {"room_name": room_name, "user_name": user_name},
-    )
+def index(request):
+    # 여기서 request는 WSGIRequest 이다
+    # 근데 위 drf의 request와 다른 객체이다!!!
+    # 이건 장고 자체 rest 인 듯..!
+    return render(request, "chat/index.html", {})
--- backend/config/settings/development.py
+++ backend/config/settings/development.py
@@ -27,6 +27,21 @@ CHANNEL_LAYERS = {
 WSGI_APPLICATION = "config.wsgi.application"
 ASGI_APPLICATION = "config.asgi_local.application"
 
+
+"""
 # CORS_ALLOW_CREDENTIALS=True 임에도 whitelist는 필요하다!
-CORS_ORIGIN_WHITELIST = ["https://localhost:8082", "http://localhost:8082"]
+# CORS_ALLOW_ALL_ORIGINS = True <- 이거 있으면 whitelist 필요 x
+CORS_ORIGIN_WHITELIST = [
+    "https://localhost:8082",
+    "http://localhost:8082",
+    "https://localhost",
+    "http://localhost",
+]
+"""
+
+CORS_ALLOW_ALL_ORIGINS = (
+    True  # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+)
 CORS_ALLOW_CREDENTIALS = True
+
+CSRF_TRUSTED_ORIGINS = ["https://localhost", "https://192.168.1.243"]
