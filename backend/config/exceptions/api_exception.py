3cfa36b7637b43576116dcd616275f7c1da842dd
park young jae <jolie0215@gmail.com>
2023-04-29 00:09:33 +0900

    Fix - exception에서 circular 오류 수정

--- backend/config/exceptions/api_exception.py
+++ backend/config/exceptions/api_exception.py
@@ -10,17 +10,16 @@ from config.exceptions.custom_exceptions import (
 from config.settings.base import logger_error
 from config.exceptions.exception_codes import STATUS_RSP_INTERNAL_ERROR
 
+import logging
 import traceback
+
+logger = logging.getLogger("django")
 
 
 def custom_exception_handler(exc, context):
-    print(traceback.format_exc())
-    print("#", str(exc))
-    print("##", repr(exc))
-
     message = {"message": str(exc)}
 
-    if "view" in context:
+    if False and "view" in context:
         if "head" in context["view"].__dict__:
             message["head"] = context["view"].__dict__["head"].__dict__
         if "headers" in context["view"].__dict__:
@@ -32,12 +31,15 @@ def custom_exception_handler(exc, context):
         if "kwargs" in context["view"].__dict__:
             message["kwargs"] = context["view"].__dict__["kwargs"]
 
-    logger_error.error(message)
+        logger_error.error(message)
+
+    logger.info(traceback.format_exc())
 
     response = exception_handler(exc, context)
 
     if response is not None:
-        status_code = 200
+        status_code = response.status_code
+        msg = exc.detail
 
         if isinstance(exc, exceptions.ParseError):
             code = response.status_code
@@ -72,38 +74,6 @@ def custom_exception_handler(exc, context):
         elif isinstance(exc, CustomParameterException):
             code = CustomParameterException.default_code
             msg = CustomParameterException.default_detail
-        elif isinstance(exc, CustomDictException):
-            """
-            APIException dictionary instance process
-            For Localization Error Control
-
-            아래와 같은 형태 필요
-            STATUS_RSP_INTERNAL_ERROR = {
-                'code': 'internal-error',
-                'default_message': 'unknown error occurred.',
-                'lang_message': {
-                    'ko': '알 수 없는 오류.',
-                    'en': 'unknown error occurred.',
-                }
-            }
-
-            CustomDictException(STATUS_RSP_INTERNAL_ERROR, {"키": "내용", "키": "내용"}) 으로 추가 가능
-            code 부분을 추가적인 내용을 넣는 방식으로 사용
-            """
-            code = exc.detail.get("code")
-
-            if hasattr(context["request"], "LANGUAGE_CODE"):
-                language_code = context["request"].LANGUAGE_CODE
-                msg = exc.detail.get("lang_message").get(language_code)
-            else:
-                msg = exc.detail.get("default_message")
-
-            if exc.args[1:]:
-                for key, val in exc.args[1].items():
-                    response.data[key] = val
-
-            response.data.pop("default_message", None)
-            response.data.pop("lang_message", None)
         elif isinstance(exc, CustomException):
             msg = exc.detail.get("message")
             code = exc.detail.get("code")
@@ -113,15 +83,13 @@ def custom_exception_handler(exc, context):
             msg = "unknown error"
 
         response.status_code = status_code
-        response.data["code"] = code
-        response.data["message"] = msg
+        response.data = {"message": msg, "code": code}
 
         ## 위 handler에서 exception내용이 response에 들어가는 것 같다.
-        response.data.pop("status_code", None)
+        # response.data.pop("status_code", None)
 
         return response
     else:
         """일반적으로 Err가 발생하면 이쪽으로 빠진다."""
         msg = {"message": str(exc), "code": 500}
-
         return Response(msg, status=500)
