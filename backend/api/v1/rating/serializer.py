e591d0c1fbca13a095b528432c8b5050e7ebddde
Park Young Jae <jolie0215@gmail.com>
2023-05-21 16:56:45 +0900

    Feat - movie basic serializer

--- backend/api/v1/rating/serializer.py
+++ backend/api/v1/rating/serializer.py
@@ -1,51 +1,62 @@
 from rest_framework import serializers
 
-from api.v1.rating.models import Movie
+from api.v1.rating.models import (
+    Nation,
+    Genre,
+    Movie,
+    MovieRating,
+    MovieRateType,
+    Actor,
+    ActorRateType,
+    ActorRating,
+)
 
 
-class NationSerilizer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(
-        required=False, allow_null=True, allow_blank=True, max_length=32
-    )
+class NationSerilizer(serializers.ModelSerializer):
+    class Meta:
+        model = Nation
+        fields = "__all__"
 
 
-class MovieRateTypeSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    rate_type_name = serializers.CharField(
-        required=False, allow_null=True, allow_blank=True, max_length=32
-    )
+class GenreSerilizer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = "__all__"
 
 
-class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    open = serializers.DateField()
-
-    nation = NationSerilizer()
-
-    created_at = serializers.DateTimeField(required=False)
-    modified_at = serializers.DateTimeField(required=False, allow_null=True)
-    deleted_at = serializers.DateTimeField(required=False, allow_null=True)
-
-    def create(self, validated_data):
-        where = self.get_where(validated_data.pop("where"))
-        movie = Movie.objects.create(where=where, **validated_data)
-        return movie
-
-    def update(self, instance, validated_data):
-        where = self.get_where(validated_data.pop("where"))
-        instance.where = where
-
-        instance.when = validated_data.get("when", instance.when)
-        instance.level = validated_data.get("level", instance.level)
-        instance.score = validated_data.get("score", instance.score)
-        instance.memo = validated_data.get("memo", instance.memo)
-        instance.picture = validated_data.get("picture", instance.picture)
-        instance.tags = validated_data.get("tags", instance.tags)
-        instance.save()
-        return instance
-
+class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = "__all__"
         exclude = ["modified_at", "deleted_at"]
+
+
+class MovieRateTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MovieRateType
+        fields = "__all__"
+
+
+class MovieRatingTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MovieRating
+        fields = "__all__"
+
+
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = "__all__"
+        exclude = ["modified_at", "deleted_at"]
+
+
+class ActorRateTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ActorRateType
+        fields = "__all__"
+
+
+class ActorRatingTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ActorRating
+        fields = "__all__"
