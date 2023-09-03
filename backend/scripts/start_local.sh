cd81b97fa0e338ac860817622c0238b01c9b7fa5
yj.park <jolie0215@gmail.com>
2023-09-03 20:48:32 +0900

    Fix - manage.py 도 하나로 변경

--- backend/scripts/start_local.sh
+++ backend/scripts/start_local.sh
@@ -2,11 +2,11 @@
 
 mkdir -p log
 
-python manage_local.py makemigrations
-python manage_local.py migrate
-python manage_local.py collectstatic --noinput --verbosity 0
+python manage.py makemigrations
+python manage.py migrate
+python manage.py collectstatic --noinput --verbosity 0
 
 #nohup service supervisor start & # 이걸 해줘야 supervisor가 정상적으로 동작한다.
 supervisord &
-python manage_local.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000
 #gunicorn -c config/gunicorn.conf.py config.wsgi:application
