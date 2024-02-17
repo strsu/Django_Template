424582cd25b6ef9c4402e7ffeede14718b28a0aa
yj.park <jolie0215@gmail.com>
2024-02-17 23:25:27 +0900

    ,

--- backend/config/admin_page/error_view.py
+++ backend/config/admin_page/error_view.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
+from django.http import HttpResponse
+
 
 def handler404(request):
     return render(request, "404.html", status=404)
 
 
 def bad_request(request, exception, *args, **kwargs):
-    data = {"error": str(exception)}
-    return JsonResponse(data, status=400)
+
+    message = str(exception)
+    # return Response(data, status=400)
+    return HttpResponse(
+        message, status=400, content_type="application/json; charset=utf-8"
+    )
 
 
 def server_error(request, *args, **kwargs):
