d4ae2eb3c5008d906a8803f1da2f1554ab082029
yj.park <jolie0215@gmail.com>
2024-05-31 04:31:50 +0900

    Fix - rolling 배포 완성

--- deploy-rolling.sh
+++ deploy-rolling.sh
@@ -2,7 +2,7 @@
 
 # cp ./nginx/nginx_A.conf ./nginx/nginx.conf
 
-# docker-compose -f docker-compose.yml -p prup build backend_a
+# docker-compose -f docker-compose-rolling.yml -p prup up -d --build
 
 
 # docker-compose -f docker-compose.yml -p prup down backend_b
@@ -13,38 +13,38 @@ EXIST_A=$(docker-compose -f docker-compose.yml -p prup ps | grep backend_a)
 
 if [ -z "$EXIST_A" ]; then
     echo "Build backend_a"
-    docker-compose -f docker-compose.yml -p prup up -d --build backend_a
+    docker-compose -f docker-compose-rolling.yml -p prup up -d --build backend_a
 
-    sleep 5
+    sleep 30
     
     echo "Change nginx.conf for backend_a"
     cp ./nginx/nginx_A.conf ./nginx/nginx.conf
 
-    sleep 1
+    sleep 30
 
     echo "Reload nginx.conf for backend_a"
-    docker-compose -f docker-compose.yml -p prup exec nginx nginx -s reload
+    docker-compose -f docker-compose-rolling.yml -p prup exec nginx nginx -s reload
 
-    sleep 10
+    sleep 30
 
     echo "Down backend_b"
-    docker-compose -f docker-compose.yml -p prup down backend_b
+    docker-compose -f docker-compose-rolling.yml -p prup down backend_b
 else
     echo "Build backend_b"
-    docker-compose -f docker-compose.yml -p prup up -d --build backend_b
+    docker-compose -f docker-compose-rolling.yml -p prup up -d --build backend_b
 
-    sleep 5
+    sleep 30
 
     echo "Change nginx.conf for backend_b"
     cp ./nginx/nginx_B.conf ./nginx/nginx.conf
 
-    sleep 1
+    sleep 30
     
     echo "Reload nginx.conf for backend_b"
-    docker-compose -f docker-compose.yml -p prup exec nginx nginx -s reload
+    docker-compose -f docker-compose-rolling.yml -p prup exec nginx nginx -s reload
 
-    sleep 10
+    sleep 30
 
     echo "Down backend_a"
-    docker-compose -f docker-compose.yml -p prup down backend_a
+    docker-compose -f docker-compose-rolling.yml -p prup down backend_a
 fi
