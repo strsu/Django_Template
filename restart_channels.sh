e9d3367fcb475762a1aa6ecdd23ad347bf180696
yj.park <jolie0215@gmail.com>
2024-05-31 04:56:28 +0900

    Chore - socket restart sh

--- restart_channels.sh
+++ restart_channels.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-docker-compose -f docker-compose.yml -p prup restart channels
+#docker-compose -f docker-compose.yml -p prup restart channels
+
+docker-compose -f docker-compose.yml -p prup exec channels /bin/bash -c "kill -HUP \$(cat /var/run/supervisord.pid)"
