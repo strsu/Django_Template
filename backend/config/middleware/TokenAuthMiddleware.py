fbb094558504cd0f7641bde2d141fcf2ce732fe4
yj.park <jolie0215@gmail.com>
2024-06-14 16:47:01 +0900

    Feat - socket token 인증 방식 추가

--- backend/config/middleware/TokenAuthMiddleware.py
+++ backend/config/middleware/TokenAuthMiddleware.py
@@ -23,25 +23,35 @@ class JWTTokenAuthMiddleware:
         # Close old database connections to prevent usage of timed out connections
         close_old_connections()
 
-        scope["user"] = "AnonymousUser"
+        scope["user"] = None
 
         try:
             cookie_header = next(
                 (value for key, value in scope["headers"] if key.lower() == b"cookie"),
                 None,
             )
+
             if cookie_header is not None:
-                cookie_header = cookie_header.decode("utf-8")
-                if "x-authorization" in cookie_header.lower():
-                    for cookie in cookie_header.split("; "):
-                        if "x-authorization" in cookie.lower():
-                            _, value = cookie.split("=")
-                            scope["user"] = await self.verify_token(value)
-                            break
+                cookies = dict(
+                    cookie.split("=")
+                    for cookie in cookie_header.decode("utf-8").split("; ")
+                )
+                cookies = {
+                    key.lower(): value for key, value in cookies.items()
+                }  # 헤더 키를 소문자로 변환
+
+                x_authorization_value = cookies.get("x-authorization")
+                token = cookies.get("token")
+
+                if x_authorization_value is not None:
+                    scope["user"] = await self.verify_jwt_token(x_authorization_value)
+                elif token is not None:
+                    scope["user"] = await self.verify_token(token)
                 else:
-                    print("X-Authorization header not found")
+                    print("Token header not found")
             else:
                 print("Cookie header not found")
+
         except Exception as e:
             print(e)
             print(traceback.format_exc())
@@ -49,17 +59,25 @@ class JWTTokenAuthMiddleware:
         return await self.app(scope, receive, send)
 
     @database_sync_to_async
-    def verify_token(self, token):
+    def verify_jwt_token(self, token):
         from rest_framework_simplejwt.authentication import JWTAuthentication
 
         try:
             jwt_authentication = JWTAuthentication()
             validated_token = jwt_authentication.get_validated_token(token)
-            user = jwt_authentication.get_user(validated_token)
-            return user
+            return jwt_authentication.get_user(validated_token)
         except Exception as e:
-            print(e)
-            return "AnonymousUser"
+            return None
+
+    @database_sync_to_async
+    def verify_token(self, token):
+        from rest_framework.authtoken.models import Token
+
+        try:
+            token_obj = Token.objects.get(key=token)
+            return token_obj.user
+        except Exception as e:
+            return None
 
 
 def JWTAuthMiddlewareStack(app):
