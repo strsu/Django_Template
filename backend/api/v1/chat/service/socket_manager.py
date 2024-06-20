2e29c1ed88d92ee9fd666e74c960bb2c88a49374
yj.park <jolie0215@gmail.com>
2024-06-20 18:44:09 +0900

    ,

--- backend/api/v1/chat/service/socket_manager.py
+++ backend/api/v1/chat/service/socket_manager.py
@@ -6,10 +6,6 @@ class SocketManager:
 
     channel_name = "prup"
 
-    @property
-    def channel_name():
-        pass
-
     @classmethod
     def _group_send_(cls, data):
         """
@@ -21,9 +17,34 @@ class SocketManager:
             -> 때문에 서버에서 type에 맞는 함수가 없으면 소켓이 죽는다
 
         """
-        async_to_sync(channels.layers.get_channel_layer().group_send)(
-            cls.channel_name, data
-        )
+        try:
+            async_to_sync(channels.layers.get_channel_layer().group_send)(
+                cls.channel_name, data
+            )
+        except Exception as e:
+            """
+            future: <Task finished name='Task-377' coro=<Connection.disconnect() done, defined at /usr/local/lib/python3.8/site-packages/redis/asyncio/connection.py:723> exception=RuntimeError('Event loop is closed')>
+            Traceback (most recent call last):
+            File "/usr/local/lib/python3.8/site-packages/redis/asyncio/connection.py", line 732, in disconnect
+                self._writer.close()  # type: ignore[union-attr]
+            File "/usr/local/lib/python3.8/asyncio/streams.py", line 353, in close
+                return self._transport.close()
+            File "/usr/local/lib/python3.8/asyncio/selector_events.py", line 692, in close
+                self._loop.call_soon(self._call_connection_lost, None)
+            File "/usr/local/lib/python3.8/asyncio/base_events.py", line 719, in call_soon
+                self._check_closed()
+            File "/usr/local/lib/python3.8/asyncio/base_events.py", line 508, in _check_closed
+                raise RuntimeError('Event loop is closed')
+            RuntimeError: Event loop is closed
+            
+            위 오류가 난다면, 
+                [ ] "channels_redis.core.RedisChannelLayer",
+                [x] "channels_redis.pubsub.RedisPubSubChannelLayer",
+            RedisPubSubChannelLayer 를 사용하면 해결할 수 있다.
+
+            위 오류가 여기 exception에서 잡히는 오류가 아니다!!
+            """
+            print(f"## - {e}")
 
     @classmethod
     def info(cls, message):
