f52f18653e0d0a00a3066fa608f0274baf074aa0
yj.park <jolie0215@gmail.com>
2024-06-23 22:05:02 +0900

    ,

--- restart_beat.sh
+++ restart_beat.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker-compose -f docker-compose-celery.yml -p celery exec celery /bin/bash -c "kill -HUP \$(cat beat.pid)"
+docker-compose -f docker-compose-celery.yml -p celery exec celery /bin/bash -c "kill -HUP \$(cat celery_beat.pid)"
