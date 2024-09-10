2868646be7a9d6021273803a7a516c602dc8427c
yj.park <jolie0215@gmail.com>
2024-09-11 01:47:12 +0900

    Feat - decimal 공부

--- backend/api/v1/orm/models.py
+++ backend/api/v1/orm/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 
-# Create your models here.
 class Price(models.Model):
     date = models.DateField()
     time = models.IntegerField()
@@ -10,3 +9,16 @@ class Price(models.Model):
 
     class Meta:
         verbose_name = "price"
+
+
+class PowerGeneration(models.Model):
+
+    power_decimal = models.DecimalField(
+        "현재출력",
+        max_digits=20,  # 전체 자리수를 20으로 설정, 20을 넘어가면 숫자가 잘린다!!!
+        decimal_places=6,
+    )
+    power_float = models.FloatField("현재출력")
+
+    class Meta:
+        verbose_name = "발전량"
---
+++ backend/api/v1/orm/tests/test_power_generation.py
@@ -0,0 +1,57 @@
+from django.test import Client, TestCase
+from django.db.models import Sum
+
+from api.v1.orm.models import PowerGeneration
+
+from decimal import Decimal, ROUND_HALF_UP, getcontext, Context
+import random
+
+getcontext().prec = 50  # 이렇게 설정하는건 의미가 없다. 해당 파일에 적용되는게 아니라 전역 변수에만 적용된다.
+
+# python manage.py test api.v1.orm.tests.test_power_generation
+# python manage.py test api/v1/orm/tests --settings='config.settings.test_real_db'
+
+
+class DecimalTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+
+        total_cnt = 10000
+        obj_list = []
+
+        for _ in range(total_cnt):
+            v = round(random.randint(1000000, 1000000000) / 1000, 6)
+            obj_list.append(PowerGeneration(power_decimal=v, power_float=v))
+
+        PowerGeneration.objects.bulk_create(obj_list, batch_size=1000)
+
+    def test_DB_합_결과(self):
+
+        pg = PowerGeneration.objects.aggregate(
+            decimal=Sum("power_decimal"), float=Sum("power_float")
+        )
+
+        # 현재의 함수의 context에 적용된다. class 밖에 설정하면 의미가 없다.
+        getcontext().prec = 20
+        getcontext().rounding = ROUND_HALF_UP
+        context = Context(prec=50, rounding=ROUND_HALF_UP)  # 이건 그냥 적용이 안된다.
+        context = None
+        v_float_sum = 0
+        v_decimal_sum = Decimal(0)
+        v_decimal_str_sum = Decimal("0")
+
+        for value in PowerGeneration.objects.all():
+            v_d = Decimal(value.power_float, context=context)
+            v_ds = Decimal(str(value.power_float), context=context)
+            v_float_sum += value.power_float
+            v_decimal_sum += v_d
+            v_decimal_str_sum += v_ds
+
+        self.assertEqual(v_float_sum, pg.get("float"))
+        self.assertEqual(v_decimal_str_sum, pg.get("decimal"))
+
+        print(
+            f"v_float_sum: {v_float_sum}, v_decimal_sum: {v_decimal_sum}, v_decimal_str_sum: {v_decimal_str_sum}"
+        )
+
+        print(pg)
