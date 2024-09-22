aebd8bd5662f4ea649eb5e45db888b6d5ae536c3
yj.park <jolie0215@gmail.com>
2024-09-22 19:35:40 +0900

    Chore - point serializer 적용

--- backend/api/v1/map/serializer.py
+++ backend/api/v1/map/serializer.py
@@ -1,1 +1,11 @@
 from rest_framework import serializers
+from rest_framework_gis.fields import GeometryField
+
+from .models import Map
+
+
+class MapSerilizer(serializers.ModelSerializer):
+    coord = GeometryField(help_text="거래장소", required=True)
+
+    class Meta:
+        model = Map
